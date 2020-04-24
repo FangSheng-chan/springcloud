867bceca1135c65e3b0ab1f14c94f11b99352174
fangsheng <445317262@qq.com>
2020-04-24 18:24:18 +0800

    one

---
+++ springcloud/cloud-provider-payment8001/src/main/java/com/ss/springcloud/entities/CommonResult.java
@@ -0,0 +1,20 @@
+package com.ss.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+//通用的json实体
+public class CommonResult<T> {
+    //404Not Found
+    private Integer code;
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message) {
+        this(code, message, null);
+    }
+}
---
+++ springcloud/cloud-provider-payment8001/src/main/java/com/ss/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ss.springcloud.service.impl;
+
+import com.ss.springcloud.dao.PaymentDao;
+import com.ss.springcloud.entities.Payment;
+import com.ss.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+
+}
