3e28feac48339bbeb526e25f2d7be32bc38cb25b
fangsheng <445317262@qq.com>
2020-04-28 01:32:41 +0800

    three

--- springcloud/cloud-consumer-order80/src/main/java/com/ss/springcloud/controller/OrderController.java
+++ springcloud/cloud-consumer-order80/src/main/java/com/ss/springcloud/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.ss.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,12 +15,12 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static final String PAYMENT_URL = "http:localhost:8001";
+    public static final String PAYMENT_URL = "http://localhost:8001";
 
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping("/consumer/payment/create")
+    @PostMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
