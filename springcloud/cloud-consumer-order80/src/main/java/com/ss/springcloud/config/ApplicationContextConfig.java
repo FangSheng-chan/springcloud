6e2f1c283e19996e64091c9ae846447fece7fe9e
fangsheng <445317262@qq.com>
2021-05-16 20:39:02 +0800

    7

--- springcloud/cloud-consumer-order80/src/main/java/com/ss/springcloud/config/ApplicationContextConfig.java
+++ springcloud/cloud-consumer-order80/src/main/java/com/ss/springcloud/config/ApplicationContextConfig.java
@@ -1,6 +1,7 @@
 package com.ss.springcloud.config;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,10 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig {
 
     @Bean
+    /**
+     * 使用 @LoadBalanced注解注释赋予 RestTemplate 负载均衡能力
+     */
+    @LoadBalanced
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
