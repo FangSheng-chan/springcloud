2b5c1969aea73e69409683ab9fd9701eec32a105
fangsheng <445317262@qq.com>
2021-05-25 23:26:43 +0800

    13

--- springcloud/cloud-provider-hystrix-payment8001/src/main/java/com/ss/springcloud/service/PaymentService.java
+++ springcloud/cloud-provider-hystrix-payment8001/src/main/java/com/ss/springcloud/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.ss.springcloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -11,10 +12,15 @@ public class PaymentService {
         return "线程池:  "+Thread.currentThread().getName()+"  paymentInfo_OK,id:  "+id+"\t"+"O(∩_∩)O哈哈~";
     }
 
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler")
     public String paymentInfo_TimeOut(Integer id)
     {
         try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): 3";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池:  "+Thread.currentThread().getName()+" paymentInfo_TimeOutHandler,id:  "+id+"\t"+"超时了"+"  耗时(秒): 3";
     }
 
 }
